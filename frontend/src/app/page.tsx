f2831df4c345ffa7d5fdfc2317b1e579c49a2c47
Harsh Mahajan <harshmahajan2345@gmail.com>
2025-03-04 23:56:52 +0530

    resized font

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,12 +9,12 @@ export default function Home() {
   return (
     <div className="overflow-hidden w-screen h-screen dark:text-white">
       <div>
-        <h1 className="p-2 bg-clip-text text-3xl font-bold text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+        <h1 className="top-0 left-0 m-4 p-1 bg-clip-text text-3xl font-bold text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
           Aword
         </h1>
       </div>
       <div className="flex flex-col justify-center items-center h-screen">
-        <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+        <h1 className="text-4xl md:text-4xl lg:text-7xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
           Chat With Your Peers <br /> at{" "}
           <Cover>
             <span className="dark:text-amber-300 text-red-500">Warp Speed</span>
